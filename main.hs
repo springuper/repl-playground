6d64a522f073026b734b7f7e39c77622da6a94c1
chun shang <springuper@gmail.com>
2020-03-23 01:59:46 +0000

    init

--- main.hs
+++ main.hs
@@ -1,1 +1,20 @@
-[ x | x <- [1..10], x `mod` 2 == 0]
+fizz :: Int -> String
+fizz n | n `mod` 15 == 0  = "FizzBuzz"
+       | n `mod` 3  == 0  = "Fizz"
+       | n `mod` 5  == 0  = "Buzz"
+       | otherwise = show n
+
+quick_sort        :: (Ord a) => [a] -> [a]
+quick_sort []     = []
+quick_sort (x:xs) = quick_sort ls ++ [x] ++ quick_sort rs
+  where
+    ls            = [l | l <- xs, l <= x]
+    rs            = [r | r <- xs, r > x]
+
+main :: IO()
+main = do
+  print "quick_sort:"
+  print (quick_sort [3, 1, 9, 8, 10])
+
+  print "fizz:"
+  mapM_ putStrLn $ map fizz [1..20]
--- quick_sort.hs
+++
@@ -1,8 +0,0 @@
-quick_sort :: (Ord d) => [a] -> [a]
-quick_sort [] = []
-quick_sort [x:xs] = quick_sort ls ++ [x] ++ quick_sort rs
-  where
-    ls = [l | l <- xs, l <= x]
-    rs = [r | r <- xs, r > x]
-
-quick_sort [3, 5, 4, 7, 1]
